7dd9893a7ed1242bad1302f4c46c69c51bd522a3
Avnerwat <pipipipi443@yahoo.com>
2026-09-20 03:41:19 +0800

    Add two report headings

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -5,6 +5,8 @@ public class mainApp1 {
     public static void main(String[] args) {
         int b = 2;
         int n = 11;
+        System.out.println("COMP3111 Lab 1");
+        System.out.println("Power and factorial demonstration");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
         System.out.println("Program ..Ended …");
